2c88e8ec81c2f8bf7bded9da68d721412b776ed9
wgrochal <wgrochal@cloudferro.com>
2019-11-22 04:47:24 +0100

    refactor DjangoObjectHandler

--- microframework/utils.py
+++ microframework/utils.py
@@ -22,6 +22,7 @@ def _model_has_rel_to(model, sorted_models):
                 return True
     return False
 
+
 def get_model_relations(model, sorted_models):
     model_fields = model._meta.get_fields()
     for model_field in model_fields:
@@ -53,3 +54,24 @@ def sort_models_by_relations(models, sorted_models=None):
             _sorted.append(model)
             _unsorted.remove(model)
     return sort_models_by_relations(_unsorted, _sorted)
+
+
+def get_field_by_name(name, model):
+    model_meta_fields = model._meta.fields
+    for field in model_meta_fields:
+        if field.name == name:
+            return field
+    return None
+
+
+def transform_serialized_foreign_fields(fields, model):
+    fields_transformed = {}
+    for field_name, field_value in fields.items():
+        field = get_field_by_name(field_name, model)
+        if field:
+            field_type = field.get_internal_type()
+            if field_type in ['ForeignKey', 'TreeForeignKey']:
+                fields_transformed[f'{field_name}_id'] = field_value
+            else:
+                fields_transformed[field_name] = field_value
+    return fields_transformed
